025c6ee69210431eb1963973c6985f56958ca8c4
sebinpalmtrix <sebin.palmtrix@gmail.com>
2020-01-23 15:26:33 +0530

    sebin

---
+++ atAcc/Models/ViewModels/AccountsParent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using atAcc.Models.DB;
+
+namespace atAcc.Models.ViewModels
+{
+    public class AccountsParent
+    {
+        public  tbl_accountLedgerDtls accountledger { get; set; }
+        public tbl_accountMoreDtls accountmore { get; set; }
+        public tbl_accountGroupDtls groupdetails { get; set; }
+    }
+}
